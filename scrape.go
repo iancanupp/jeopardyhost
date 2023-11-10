cdaaf9e37e3a67b45546fee73c432ea4506bd28d
Ian Canupp <canuppian@gmail.com>
2023-11-09 18:25:18 -0800

    merged the two endpoints into one, saved unique answers

--- scrape.go
+++ scrape.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
 )
@@ -10,20 +14,66 @@ import (
 var today *finaljeop
 var random *finaljeop
 
+/*
+cat: The category of the question
+clue: The question itself
+answer: The correct answer to the question
+id: the randomly generated ID that will be used to look up the answer
+url: the generated url that the correct answer to this particular question will be saved at
+*/
 type finaljeop struct {
 	category string
 	clue     string
 	answer   string
+	id       int
+	url      string
 }
 
+/*
+Creates a new Final Jeopardy Question and saves every answer
+*/
 func newFinalJeop(cat string, clue string, answer string) *finaljeop {
 
 	fj := finaljeop{category: cat}
 	fj.clue = clue
 	fj.answer = answer
+
+	// random 6 digit ID for answer URL TKTKTK clear this every x minutes
+	rand.Seed(time.Now().UnixNano())
+	min := 99999
+	max := 1000000
+	randomID := rand.Intn(max-min+1) + min
+	fj.id = randomID
+	fj.url = "localhost:1964/answer?param=" + strconv.Itoa(randomID)
+
+	rememberThat(randomID, answer, clue)
+
 	return &fj
 }
 
+func rememberThat(id int, answer string, clue string) {
+	f, err := os.Create(strconv.Itoa(id) + "answer.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	l, err := f.WriteString(answer + "~!~" + clue)
+	if err != nil {
+		fmt.Println(err, l)
+		f.Close()
+		return
+	}
+
+	// fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// Deprecated
 func didYouGetHere() {
 	fmt.Println("You made it this far")
 }
@@ -49,7 +99,6 @@ func scrape() {
 	c.OnHTML(".final_round", func(h *colly.HTMLElement) {
 		fullTable := h.DOM
 		catName := fullTable.Find(".category_name").Text()
-		// fmt.Println(catName)
 
 		// This might look a little weird to you in the future, but the clue_text div applies to the clue itself and the hidden answer underneath
 		// In order to separate them into two separate entities, we just remove the answer from the clue string itself
@@ -69,6 +118,6 @@ func scrape() {
 
 	c.Visit("https://j-archive.com/")
 
-	// fmt.Println("Today's", today)
+	// fmt.Println("Today's: ", today)
 	// fmt.Println("Random:", random)
 }
--- service.go
+++ service.go
@@ -1,34 +1,107 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func Hello(rw http.ResponseWriter, r *http.Request) {
-	/*
-	 * The response writer is what the server will respond with for any request. That's how you write text, json or whatever you want to return to the client
-	 * The request includes things like headers and cookies and anything else that the client (web browser) sends to the server
-	 */
-	rw.Write([]byte("Hello"))
+/*
+Function to route to either the final jeopardy question du jour, or grab a randomized
+one from the archives
+*/
+func placeYourWagers(rw http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query().Get("param")
+	// for k, v := range values {
+	// 	fmt.Println(k, " => ", v)
+	// }
+	if strings.Contains(values, "today") {
+		takeItAwayKen(rw)
+	} else {
+		hitTheTrebekVault(rw)
+	}
 }
 
-// I want the answer to be returned in link markdown that takes you to a simple page that displays the answer
-func takeItAwayKen(rw http.ResponseWriter, r *http.Request) {
-	scrape()
-	rw.Write([]byte(today.clue))
+/*
+Opens up the corresponding file from the baked-in answerID in the url,
+then calls the helper to format it it for display
+*/
+func displayAnswer(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	answerID := r.URL.Query().Get("param")
+
+	contents, err := os.ReadFile(answerID + "answer.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	rw.Write([]byte(formatAnswerAsHTML(string(contents))))
 }
 
-func hitTheTrebekVault(rw http.ResponseWriter, r *http.Request) {
+/*
+Helper to the reading file function that takes the raw file contents
+and splits it into answer/clue in the html for display
+*/
+func formatAnswerAsHTML(contents string) string {
+	splitOnComma := strings.Split(contents, "~!~")
+	answer := splitOnComma[0]
+	clue := splitOnComma[1]
+
+	htmlStr := "<h1 style='color: blue'>What is... " + answer + "</h1>\n" + "<p>was the correct answer to the clue..." + clue + "</p>"
+
+	return htmlStr
+}
+
+/*
+Route taken when the request is for today's final jeopardy question.
+Otherwise, does the same thing as the randomized one
+*/
+func takeItAwayKen(w http.ResponseWriter) {
 	scrape()
-	rw.Write([]byte(random.clue))
+	strbuffer := "In the category " + today.category + ", your clue is:\n" + today.clue + "\n[Time's up! Reveal answer](" + today.url + ")"
+	w.Write([]byte(strbuffer))
+}
+
+/*
+Route taken when the request is missing an indication of 'today', this will
+bring up a random final jeopardy
+*/
+func hitTheTrebekVault(w http.ResponseWriter) {
+	// definitely do a separate file of lead ins to category and clue here"
+	scrape()
+
+	strbuffer := "In the category " + random.category + ", your clue is:\n" + random.clue + "\n[Time's up! Reveal answer](" + random.url + ")"
+	w.Write([]byte(strbuffer))
+
+	// This was for testing the load answer function in html instead of markdown
+	// w.Header().Set("Content-Type", "text/html")
+	// urlhtml := createLinkHTML(random.url)
+	// w.Write([]byte("<p>" + random.category + "</p><p>" + random.clue + "</p><p>" + urlhtml + "</p>"))
+
+}
+
+// This was for testing the load answer function in html instead of markdown
+func createLinkHTML(url string) string {
+	// print string with double quotes
+	fmt.Printf(`"%s"
+`, url)
+
+	// create string with double quotes
+	quotedURL := fmt.Sprintf(`"%s"`, url)
+
+	finalStr := "<a href=" + quotedURL + ">Answer</a>"
+	return finalStr
 }
 
 func main() {
-	http.HandleFunc("/todayjeop", takeItAwayKen)
-	http.HandleFunc("/jeop", hitTheTrebekVault)
+	http.HandleFunc("/jeop", placeYourWagers)
+	http.HandleFunc("/answer", displayAnswer)
+	// TKTKTKTK maybe a help endpoint later
 
-	didYouGetHere()
+	// didYouGetHere()
 
 	log.Fatal(http.ListenAndServe(":1964", nil))
 }
